02d54cb3f58bc44ec0dc7e695320999a58217335
DownloadableFox <me@downloadablefox.dev>
2026-03-05 20:53:47 -0500

    better font

--- display_ip/driver.py
+++ display_ip/driver.py
@@ -1,17 +1,19 @@
 import board
 import busio
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
 _i2c = None
 _oled = None
 _image = None
 _canvas = None
+_font = None
 
 def init():
-    global _i2c, _oled, _image
+    global _i2c, _oled, _image, _font
     _i2c = busio.I2C(board.SCL, board.SDA)
     _oled = adafruit_ssd1306.SSD1306_I2C(128, 32, _i2c)
+    _font = ImageFont.load_default(size=20)
 
 def clear():
     global _oled, _image, _canvas
@@ -19,7 +21,7 @@ def clear():
     _canvas = ImageDraw.Draw(_image)
 
 def print(position, text):
-    _canvas.text((position), text, fill=255)
+    _canvas.text((position), text, font=_font, fill=255)
 
 def flush():
     _oled.image(_image)
